ffec5ce8a9c1d2086f809200eeb4820808a8ecee
ZhouNLP <66728392+ZhouNLP@users.noreply.github.com>
2022-01-19 19:03:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import os
-
+import torch
 from douzero.dmc import parser, train
 
 if __name__ == '__main__':
+    torch.multiprocessing.set_start_method('spawn')
     flags = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = flags.gpu_devices
     train(flags)
